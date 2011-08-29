78ccf1ae32fead50ed943e2c56d355909511f5fd
headz <headz@motherless.com>
2011-08-29 16:26:16 -0400

    Initial import

---
+++ test/utils.js
@@ -0,0 +1,13 @@
+var testCase = require('nodeunit').testCase;
+var utils = require('../lib/utils');
+var path = require('path');
+
+module.exports = testCase({
+	testTempnam: function(test) {
+		var file_name = utils.tempnam('/tmp', 'foo');
+		path.exists(file_name, function(exists) {
+			test.ok(exists);
+			test.done();
+		});
+	}
+});
