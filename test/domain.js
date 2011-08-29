07024c78e8fc75ec40c5c7371d3a317294168135
headz <headz@motherless.com>
2011-08-28 21:57:20 -0400

    All tests written and working

--- test/domain.js
+++ test/domain.js
@@ -1,34 +1,76 @@
 var testCase = require('nodeunit').testCase;
 var mogile = require('../index');
-require('headz');
 
 module.exports = testCase({
 	setUp: function(callback) {
-		this.trackers = [];
-		this.trackers.push('mogtracker1.int.hidsrv.net:7001');
+		this.trackers = ['mogtracker1:7001'];
+		this.client = mogile.createClient(this.trackers);
+		this.domain = this.client.domain('default');
 		callback();
 	},
 	testGetDomains: function(test) {
-		var m = mogile.factory(this.trackers);
-		m.getDomains(function(e, domains) {
+		this.client.getDomains(function(e, domains) {
 			test.ok(domains.length > 0);
 			test.done();
 		});
 	},
+	testStoreFile: function(test) {
+		this.domain.storeFile('AAAAAAA.jpg', 'dropbox', '/tmp/AAAAAAA.jpg', function(err, bytes_written) {
+			if (err) {
+				console.log(err);
+				test.ok(false);
+			} else {
+				test.ok(bytes_written > 0);
+			}
+			test.done();
+		});
+	},
 	testGetPaths: function(test) {
-		var m = mogile.factory(this.trackers);
-		var d = m.domain('motherless');
-		d.getPaths('AAAAAAA.jpg', 0, function(err, paths) {
-			test.ok(paths.length > 0);
+		this.domain.getPaths('AAAAAAA.jpg', 0, function(err, paths) {
+			if (err) {
+				test.ok(false);
+			} else {
+				test.ok(paths.length > 0);
+			}
 			test.done();
 		});
 	},
 	testGetFile: function(test) {
-		var m = mogile.factory(this.trackers);
-		var d = m.domain('motherless');
-		d.getFile('B019F32.flv', '/home/headz/site/temp/B019F32.flv', function(err, bytes) {
-			test.ok(bytes > 0);
+		this.domain.getFile('AAAAAAA.jpg', '/tmp/AAAAAAA.jpg', function(err, bytes) {
+			if (err) {
+				test.ok(false);
+			} else {
+				test.ok(bytes > 0);
+			}
 			test.done();
+		});
+	},
+	testRenameFile: function(test) {
+		this.domain.rename('AAAAAAA.jpg', 'BBBBBBB.jpg', function(err) {
+			if (err) {
+				test.ok(false);
+			} else {
+				test.ok(true);
+			}
+			test.done();
+		});
+	},
+	testDeleteFile: function(test) {
+		var $this = this;
+		this.domain.del('BBBBBBB.jpg', function(err) {
+			if (err) {
+				test.ok(false);
+			} else {
+				test.ok(true);
+			}
+			$this.domain.getPaths('BBBBBBB.jpg', 0, function(err, paths) {
+				if (!err) {
+					test.ok(false);
+				} else if (err.indexOf(err, 'unknown_key') != -1) {
+					test.ok(true);
+				}
+				test.done();
+			});
 		});
 	}
 });
